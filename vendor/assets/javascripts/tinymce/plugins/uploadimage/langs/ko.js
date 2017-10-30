28e67747fec3455fab23ddba99bfbe26be77c02e
Chogyuwon <cgw0280@gmail.com>
2017-10-31 04:50:21 +0900

    Add files via upload

---
+++ vendor/assets/javascripts/tinymce/plugins/uploadimage/langs/ko.js
@@ -0,0 +1,11 @@
+tinymce.addI18n('ko', {
+  'Insert an image from your computer': '이미지 삽입',
+  'Insert image': "이미지 삽입",
+  'Choose an image': "이미지 선택",
+  'You must choose a file': "삽입할 이미지를 선택해주세요.",
+  'Got a bad response from the server': "서버 에러",
+  "Didn't get a response from the server": "서버 에러",
+  'Insert': "삽입",
+  'Cancel': "취소",
+  'Image description': "이미지 설명",
+});
